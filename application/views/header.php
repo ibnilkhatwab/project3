b35b679146b42c57a111b2fea416c8a0cce4ab66
khatwab <ibnilkhatwab@gmail.com>
2020-04-10 02:46:06 -0700

    add file

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>SKONGA</title>
+        <title>SKONG</title>
         <link rel="stylesheet" type="text/css" href="<?= base_url() ?>bootstrap/css/bootstrap.css">
         <script type="text/javascript" src="<?= base_url() ?>bootstrap/js/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="<?= base_url() ?>bootstrap/js/bootstrap.js"></script>
